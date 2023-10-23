7618391e24e0f9ab5eed02ca9d1fa240b124b620
Jacob Chademwiri <jchademwiri@gmail.com>
2023-10-23 08:14:08 +0200

    from main to section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Hero from '@/components/Hero';
 
 const Home = () => {
   return (
-    <main className='overflow-hidden'>
+    <section className='overflow-hidden'>
       <div className='relative h-full w-full'>
         <div className='absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]' />
         <div className='absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]' />
       </div>
       <Hero />
       <Featured />
-    </main>
+    </section>
   );
 };
 export default Home;
